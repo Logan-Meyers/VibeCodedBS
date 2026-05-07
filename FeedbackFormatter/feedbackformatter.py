21eeaa39e43d491f431debd382550a5ddded9ced
Logan Meyers <theterrarian03@gmail.com>
2026-05-06 18:13:48 -0700

    bumped pa version to 8

--- FeedbackFormatter/feedbackformatter.py
+++ FeedbackFormatter/feedbackformatter.py
@@ -4,7 +4,7 @@ import sys
 import textwrap
 
 # ── Config ─────────────────────────────────────────────────────────────────────
-PA_NUMBER   = 7
+PA_NUMBER   = 8
 INPUT_FILE  = os.path.expanduser("~/Desktop/feedback.csv")
 OUTPUT_FILE = os.path.expanduser(f"~/Desktop/pa{PA_NUMBER}_feedback.txt")
 
